b0a100e69f9e408fbbd6c3d05156a311bc484d23
Radithia <141038766+dith08@users.noreply.github.com>
2025-12-19 09:03:52 +0700

    Feat [RA] Setup React TS + Tailwind

---
+++ FinBits/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-sky-400 underline">
+        Mantap! Tailwind udah jalan, cuy!
+      </h1>
+    </div>
+  )
+}
+
+export default App
